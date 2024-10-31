1ef5677b2a9a9d20e0835a0f56baa9f8d1c48c68
Sudeep <sudeep7217@gmail.com>
2024-10-31 12:54:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { TracingBeamDemo }  from "./sections/Tracing"
+import TimelineDemo from "./sections/Time"
+
+function App() {
+  return (
+    <div className="text-green-600">App
+    <div className="bg-black">
+    <TracingBeamDemo />
+    </div>
+    <div className="bg-black">
+    <TimelineDemo />
+    </div>
+    </div>
+  )
+}
+
+export default App
